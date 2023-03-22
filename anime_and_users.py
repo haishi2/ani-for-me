5a3be35da4519908be530d08bc94cc1c0132e051
Shi Hai <shihai797@gmail.com>
2023-03-22 16:58:04 -0400

    added all function headers

--- anime_and_users.py
+++ anime_and_users.py
@@ -1,6 +1,7 @@
 """copyright and usage info here"""
 import datetime
 from typing import Optional
+from __future__ import annotations
 
 import python_ta
 
@@ -24,11 +25,17 @@ class Anime:
     genres: list[str]
     air_dates: tuple[datetime.date, datetime.date]
     UID: int
-    reviews: list[Review]
+    reviews: dict[User, Review]
 
     def __init__(self, title: str, num_episodes: int, genres: list[str],
                  air_dates: tuple[datetime.date, datetime.date], UID: int):
         """Initialize a new anime"""
+        self.title = title
+        self.num_episodes = num_episodes
+        self.genres = genres
+        self.air_dates = air_dates
+        self.UID = UID
+        self.reviews = {}
 
 class User:
     """A class representing a user node in the ReccomenderTree
@@ -45,50 +52,95 @@ class User:
         - len(priorities) == 5
     """
     username: str
-    reviews: list[Review]
-    favorite_animes: list[int]
+    reviews: dict[Anime, Review]
+    favorite_animes: list[Anime]
     matching_genres: list[str]
-    friends_list: list[int]
+    friends_list: list[User]
     #priorities contains most of the categories of a review ('Story', 'Animation', 'Sound', 'Character') and an
     # average of the amount of episodes from the user's favorite anime to get their preferred amount of episodes
     priorities: dict[str, int]
     weights: dict[str, float]
     favorite_era: tuple[datetime.date, datetime.date]
-    #reviews are optional since they could be loaded in from a users csv file
-    def __init__(self, username: str, favorite_animes: list[int],friends_list: list[int],
-                 priorities: dict[str, int], favorite_era: tuple[datetime.date, datetime.date],
-                 reviews: Optional[list[Review]] = None) -> None:
+
+    #reviews, priorities, and friends_list are optional since they could be loaded in from a users csv file, the regular
+    #database users don't have these properties
+    #TODO   given priority in this function only has the categories, not the avgd number of episodes
+    def __init__(self, username: str, favorite_animes: list[Anime],
+                 favorite_era: Optional[tuple[datetime.date, datetime.date]] = None,
+                 reviews: Optional[dict[Anime, Review]] = None, priorities: Optional[dict[str, int]] = None,
+                 friends_list: Optional[list[User]] = None) -> None:
         """intialize a new user and calculate their priority weights
         Preconditions:
             -
         """
-        raise NotImplementedError
+        self.username = username
+        self.favorite_animes = favorite_animes
+        if friends_list is None:
+            self.friends_list = []
+        else:
+            self.friends_list = friends_list
+        if favorite_era is None:
+            self.favorite_era = tuple()
+        self.favorite_era = favorite_era
+        if reviews is None:
+            self.reviews = {}
+        else:
+            self.reviews = reviews
+        if priorities is None:
+            self.priorities = {}
+            matching_genres = []
+            weights = {}
+        else:
+            self.priorities = priorities
+            self.calculate_genre_match_and_calculate_avg()
+            self.calculate_priority_weights()
 
-    #visited_nodes should be list[Anime | User] or list[Review]
-    def get_all_path_scores_helper(self, depth, visited_nodes: list) -> list[list[Review]]:
+
+    def get_all_path_scores_helper(self, depth, visited_nodes: list[Anime | User]) -> list[list[Review]]:
         """Helper function for get_all_path_scores that calculates all the paths
         Preconditions:
             - depth >= 1
         """
         raise NotImplementedError
 
-    def calculate_genre_match(self) -> None:
-        """Calculate the shared genres across the user's favorite anime and reviews
+    def calculate_genre_match_and_calculate_avg(self) -> None:
+        """Calculate the genres in at least 50% of the anime across the user's favorite anime and reviews
         Preconditions:
-            -
+            - favorite anime isn't empty or reviews isnt empty
         """
-        raise NotImplementedError
+        num_animes = len(self.favorite_animes) + len(self.reviews)
+        genres_count = {}
+        episodes_count = 0
+        for anime in self.favorite_animes:
+            episodes_count += anime.num_episodes
+            for genre in anime.genres:
+                if genre not in genres_count.keys():
+                    genres_count[genre] = 1
+                else:
+                    genres_count[genre] += 1
+
+        for anime in self.reviews:
+            episodes_count += anime.num_episodes
+            for genre in anime.genres:
+                if genre not in genres_count.keys():
+                    genres_count[genre] = 1
+                else:
+                    genres_count[genre] += 1
+
+        self.matching_genres = [genre for genre in genres_count if genres_count[genre] >= (num_animes / 2)]
+        self.priorities['num_episodes'] = int(episodes_count / num_animes)
+
 
     def calculate_priority_weights(self) -> None:
-        """calculate the priority weights for each category in priority except for num_episodes"""
-        raise NotImplementedError
-
-    def add_friends(self, friend_user: str) -> None:
-        """Connect this user and the friend_user together
+        """Calculate the priority weights for each category in priority except for num_episodes
         Preconditions:
             -
         """
-        raise NotImplementedError
+        #this should sum up to 100% because its taking parts out of the sum for each as their percentage share weight
+        total = sum(self.priorities.values()) - self.priorities['num_episodes']
+        for priority in self.priorities:
+            if(priority != 'num-episodes'):
+                self.weights[priority] = self.priorities[priority] / total
 
     def calculate_similarity_rating(self, anime) -> float:
         """Calculate a similarity rating between 1 and 10 to give a prediction for how much the user will like the anime
--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
 """copyright and usage info here"""
+import datetime
+
 import python_ta
 
 from anime_and_users import User, Anime
@@ -13,12 +15,18 @@ class Review:
     """
     endpoints: tuple[User, Anime]
     ratings: dict[str, int]
+
     def __init__(self, e1: User, e2: Anime, ratings: dict[str, int]):
         """Add a review and connect the two endpoints
         Preconditions:
             - user and anime both need to exist in the ReccomenderGraph
+            - the review needs to not be in the current reviews in the tree
         """
-        raise NotImplementedError
+        self.ratings = ratings
+        self.endpoints = (e1, e2)
+        e1.reviews[e2] = self
+        e2.reviews[e1] = self
+
 class ReccomenderGraph:
     """A class for a graph of nodes, where the nodes are users and animes, and edges are reviews
 
@@ -26,30 +34,40 @@ class ReccomenderGraph:
     - users: a list of user nodes
     - animes: a list of anime nodes
     """
-    users: list[User]
-    animes: list[Anime]
+    users: dict[str, User]
+    animes: dict[int, Anime]
     def __init__(self) -> None:
         """initialize an empty ReccomenderGraph
         Preconditions:
             -
         """
-        self.users = []
-        self.animes = []
+        self.users = {}
+        self.animes = {}
 
-    #check if we should be passing in a new user, or the attributes of a user
-    def insert_user(self, user: User) -> None:
+
+    def insert_user(self, user: User) -> User:
         """Add a user into the graph
         Preconditions:
             -
         """
-        raise NotImplementedError
+        self.users[user.username] = user
+        return user
 
-    def insert_anime(self, anime: Anime) -> None:
+    def insert_anime(self, anime: Anime) -> Anime:
         """Add an anime into the graph
         Preconditions:
             -
         """
-        raise NotImplementedError
+        self.animes[anime.UID] = anime
+        return anime
+
+    def add_friends(self, user: str, friend_user: str) -> None:
+        """Connect this user and the friend_user together
+        Preconditions:
+            - both users exist in the graph
+        """
+        self.users[user].friends_list.append(self.users[friend_user])
+        self.users[friend_user].friends_list.append(self.users[user])
 
     def get_all_path_scores(self, depth: int) -> dict[Anime: float]:
         """Find all anime at a certain depth and calculate a path score for each anime based on
@@ -70,13 +88,47 @@ class ReccomenderGraph:
         #remember to average the caluclated path score with the similarity rating for the anime at the endpoint
         raise NotImplementedError
 
-
+#read files in this order: anime, user, reviews
+#remember that the users in csv files dont have friends_list or priorities, so use the (param) = notation when making
+#user classes
 def read_file(files: list[str]) -> ReccomenderGraph:
     """Creates a ReccomenderGraph given the animes. profiles, and reviews formatted in a CSV file
     Preconditions:
             - files are formatted correctly
+            - files[0] is the anime file, files[1] is the user file, files[2] is the reviews file
     """
-    raise NotImplementedError
+    graph = ReccomenderGraph()
+    with open(files[0], 'r',
+              encoding="utf-8") as reader:
+        line = reader.readline()
+
+        while line != '':
+            lines = line.split(',')
+            anime_id = lines[0]
+            title = lines[1]
+            genres = []
+            i = 2
+            while all(char.isalpha() for char in lines[i]):
+                genres.append(lines[i])
+                i += 1
+
+            start_date = datetime.datetime.strptime(lines[i], '%m/%d/%Y')
+            end_date = datetime.datetime.strptime(lines[i + 1], '%m/%d/%Y')
+            num_episodes = int(lines[i + 2])
+            graph.insert_anime(Anime(title, num_episodes, genres, (start_date, end_date), int(anime_id)))
+            line = reader.readline()
+
+    with open(files[1], 'r',
+              encoding="utf-8") as reader:
+        line = reader.readline()
+
+        while line != '':
+            lines = line.split(',')
+            username = lines[0]
+            for i in range(1, len(lines)):
+
+
+
 
 
 def import_profile(file: str, graph: ReccomenderGraph) -> None:
@@ -84,10 +136,14 @@ def import_profile(file: str, graph: ReccomenderGraph) -> None:
     Preconditions:
         - files are formatted correctly
         - user doesn't exist in graph
+        - every anime in the user csv file exists in the graph
+        - avery user in the friends_list exists
     """
     raise NotImplementedError
 
 
+#write reviews in as anime name, ratings alternating for every review so can read back in and access the animes by ids
+#and create new reviews
 def save_profile(user: User, file_name: str) -> None:
     """save the user's profile into a csv file
     Preconditions:

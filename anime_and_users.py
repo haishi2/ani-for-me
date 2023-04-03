b6ee42df7c441a78312e039eb7db7db170017057
amelia1248 <128756325+amelia1248@users.noreply.github.com>
2023-04-02 21:28:58 -0400

    fixed line length

--- anime_and_users.py
+++ anime_and_users.py
@@ -92,8 +92,8 @@ class Anime:
 
         return {section: round(ratings_dict[section] / len(self.reviews), 2) for section in ratings_dict}
 
-    def get_all_path_scores_helper(self, depth: int, visited_nodes: list[Anime | User], added_ends: list[Anime | User]) -> \
-            list[list[g.Review]]:
+    def get_all_path_scores_helper(self, depth: int, visited_nodes: list[Anime | User],
+                                   added_ends: list[Anime | User]) -> list[list[g.Review]]:
         """Helper function for get_all_path_scores that calculates all the paths
         Preconditions:
             - depth >= 2
@@ -191,8 +191,8 @@ class User:
             self.calculate_genre_match_avg()
             self.calculate_priority_weights()
 
-    def get_all_path_scores_helper(self, depth: int, visited_nodes: list[Anime | User], added_ends: list[Anime | User]) -> \
-            list[list[g.Review]]:
+    def get_all_path_scores_helper(self, depth: int, visited_nodes: list[Anime | User],
+                                   added_ends: list[Anime | User]) -> list[list[g.Review]]:
         """Helper function for get_all_path_scores that calculates all the paths
         Preconditions:
             - depth >= 2

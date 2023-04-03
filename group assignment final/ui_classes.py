6dd1455f102ff0893212ad21bd0fac214ff836d5
amelia1248 <128756325+amelia1248@users.noreply.github.com>
2023-04-02 23:59:15 -0400

    Some docstrings done, saving for safety

--- group assignment final/ui_classes.py
+++ group assignment final/ui_classes.py
@@ -1,4 +1,11 @@
-""" UI Elements """
+"""
+CSC111 Project: User Interface classes
+
+This module contains the classes necessary to create and run the user interface
+that the user will interact with in the Ani-for-Me recommendation system. 
+
+This file is Copyright (c) 2023 Hai Shi, Liam Alexander Maguire, Amelia Wu, and Sanya Chawla.
+"""
 
 from typing import Optional
 
@@ -15,7 +22,8 @@ Colour = tuple[int, int, int]
 
 
 class Button:
-    """Button class"""
+    """A class that represents a button in the UI.
+    """
     _screen: pygame.Surface
     _height: Coord
     _width: Coord
@@ -35,6 +43,8 @@ class Button:
                  colour: Colour, hover_colour: Colour, text_colour: Colour, font_style: Optional[str] = None,
                  border_colour: Optional[Colour] = None,
                  border_radius: int = 0, image: Optional[pygame.Surface] = None, is_centered_text: bool = True) -> None:
+        """Initialize a Button object.
+        """
         self._screen = screen
         self._height = height
         self._width = width
@@ -57,6 +67,8 @@ class Button:
         self._is_centered_text = is_centered_text
 
     def draw(self) -> None:
+        """Draw a button on a pygame screen.
+        """
         btn_rect = pygame.Rect(self.position, (self._width, self._height))
         pygame.draw.rect(self._screen, self._current_colour, btn_rect, border_radius=self._border_radius)
         if self._border_colour is not None:
@@ -81,12 +93,15 @@ class Button:
             self._screen.blit(self.image, (img_x, img_y))
 
     def _is_hovered(self, mouse_pos: Position) -> bool:
-        """Return whether the button is being hovered"""
+        """Return whether the button is being hovered."""
         is_hover_x = self.position[0] < mouse_pos[0] < self.position[0] + self._width
         is_hover_y = self.position[1] < mouse_pos[1] < self.position[1] + self._height
         return is_hover_y and is_hover_x
 
     def update_colour(self, mouse_pos) -> bool:
+        """Return whether the button is being hovered over and
+        change colour if it is. 
+        """
         if self._is_hovered(mouse_pos):
             self._current_colour = self._hover_colour
             r_val = True
@@ -97,12 +112,13 @@ class Button:
         return r_val
 
     def is_clicked(self, is_mouse_down: bool, mouse_pos: Position):
-        """Return whether the button is clicked"""
+        """Return whether the button is clicked."""
         return self._is_hovered(mouse_pos) and is_mouse_down
 
 
 class FivePointGraph:
-    """Five point Graph for displaying rating"""
+    """Five point graph for displaying rating.
+    """
     screen: pygame.Surface
     ratings: Optional[list[int]]
     _size: Coord
@@ -120,6 +136,8 @@ class FivePointGraph:
                  size: Coord, position: Position, colour_one: Colour,
                  colour_two: Colour, dot_colour: Colour, line_colour: Colour,
                  graph_colour: tuple[int, int, int, int], rating_alpha: int, font_style: str) -> None:
+        """ Initialize a FivePointGraph object.
+        """
         self.screen = screen
         self.ratings = ratings
         self._size = size
@@ -132,7 +150,10 @@ class FivePointGraph:
         self.rating_alpha = rating_alpha
         self.font_style = font_style
 
-    def draw(self):
+    def draw(self) -> None:
+        """Draw a graph that displays ratings for 
+        5 categories(animation, sound, character, enjoyment, story).
+        """
         radius = self._size / 2
 
         # Draw the empty graph
@@ -155,6 +176,9 @@ class FivePointGraph:
                                     legend_points[i][1]))
 
     def update(self, ratings: list[float]) -> None:
+        """Update the drawn graph that displays ratings 
+        with a new list of ratings. 
+        """
         self.draw()
         # Draw the anime's rating
         radius = self._size / 2
@@ -182,7 +206,7 @@ class FivePointGraph:
         self.screen.blit(rating_surf, self.position)
 
     def get_points(self, position: complex) -> list[Position]:
-        """Return the points of the graph's nodes"""
+        """Return the points of the graph's nodes."""
         theta = 2 * pi / 5
         points = []
         for _ in range(5):
@@ -193,7 +217,7 @@ class FivePointGraph:
         return points
 
     def _get_rating_points(self, position: complex, ratings: list[float]) -> tuple[list[Position], list[Position]]:
-        """Return the position on the screen that the ratings should appear on the graph"""
+        """Return the position on the screen that the ratings should appear on the graph."""
         theta = 2 * pi / 5
         points = []
         for _ in range(5):
@@ -216,6 +240,9 @@ class FivePointGraph:
 
 
 class AnimeSpotlight:
+    """An object representing a selected anime 
+    from given recommendations. 
+    """
     screen: pygame.Surface
     background_colour: Colour
     position: Position
@@ -235,7 +262,9 @@ class AnimeSpotlight:
                  top_bar_height_percentage: float, width_percentage: float,
                  anime_colour: Colour, colour_one: Colour, colour_two: Colour,
                  dot_colour: Colour, line_colour: Colour, graph_colour: tuple[int, int, int, int], rating_alpha: int,
-                 font_style: str, title_font_size: int):
+                 font_style: str, title_font_size: int) -> None:
+        """Initialize an AnimeSpotlight object.
+        """
         self.screen = screen
         self.background_colour = background_colour
         self.position = (20, screen.get_height() * top_bar_height_percentage)
@@ -262,19 +291,25 @@ class AnimeSpotlight:
         self.curr_font_size = self.font_size
         self.title_font = pygame.font.SysFont(font_style, self.font_size, bold=False)
 
-    def draw(self):
+    def draw(self) -> None:
+        """Draw a rectangular object that stores the AnimeSpotlight object.
+        """
         anime_spotlight_rect = pygame.Rect(self.position, (self.width, self.height))
         pygame.draw.rect(self.screen, self.background_colour, anime_spotlight_rect)
         self.graph.draw()
 
-    def redraw(self):
+    def redraw(self) -> None:
+        """Redraw the graph attribute with ratings.
+        """
         self.draw()
         self.format_and_display_title()
         rating_order = ['overall', 'animation', 'sound', 'character', 'enjoyment', 'story']
         ratings = [self.ratings[rating_order[i]] for i in range(6)]
         self.graph.update(ratings)
 
-    def update(self, anime: Anime):
+    def update(self, anime: Anime) -> None:
+        """Update graph attribute with a given Anime object.
+        """
         self.anime = anime.get_title()
         # self.ratings = list(anime.ratings.values()) Use this if ratings attribute is a dict
         self.ratings = anime.calculate_average_ratings()
@@ -284,7 +319,10 @@ class AnimeSpotlight:
         ratings = [self.ratings[rating_order[i]] for i in range(6)]
         self.graph.update(ratings)
 
-    def format_and_display_title(self):
+    def format_and_display_title(self) -> None:
+        """A method that formats and displays a title
+        according to the object's attributes.
+        """
         self.title_font = pygame.font.SysFont(self.font_style, self.font_size, bold=False)
         formatted_lines = self.format_title_lines()
         self.adjust_font_size(formatted_lines)
@@ -293,7 +331,7 @@ class AnimeSpotlight:
 
     def format_title_lines(self) -> list[str]:
         """Takes a title to be rendered and returns a list of string where each string is a segment of the original
-        to be rendered so that each new line fits to the screen
+        to be rendered so that each new line fits to the screen.
         """
         final_lines = []
         title = self.anime
@@ -318,7 +356,7 @@ class AnimeSpotlight:
         return final_lines
 
     def adjust_font_size(self, lines: list[str]) -> None:
-        """Decreases the font size (self.anime_font) to ensure the title fits"""
+        """Decreases the font size (self.anime_font) to ensure the title fits."""
         self.curr_font_size = self.font_size
         self.update_font()
         num_lines = len(lines)
@@ -338,10 +376,13 @@ class AnimeSpotlight:
         self.update_font()
 
     def update_font(self) -> None:
+        """A method that updates the title font according to
+        self.font_style and self.curr_font_size.
+        """
         self.title_font = pygame.font.SysFont(self.font_style, self.curr_font_size, bold=False)
 
     def display_formatted_text(self, lines: list[str]) -> None:
-        """Helper function that neatly displays the formatted lines"""
+        """Helper method that neatly displays the formatted lines."""
         text_renders = {}
         # total_title_height = 0
         for index, line in enumerate(lines):
@@ -360,6 +401,9 @@ class AnimeSpotlight:
 
 
 class RecommendationDisplay:
+    """A class that represents the
+    display for final anime recommendations in pygame.
+    """
     screen: pygame.Surface
     width: Coord
     height: Coord
@@ -380,7 +424,9 @@ class RecommendationDisplay:
     def __init__(self, screen: pygame.Surface, top_bar_height_percentage: float,
                  width_percentage: float, anime_button_colour: Colour, anime_button_hover_colour: Colour,
                  title_colour: Colour, generate_button_colour: Colour, generate_button_hover_colour: Colour,
-                 recommendation_text_colour: Colour, font_style: str, section_title_colour: Colour):
+                 recommendation_text_colour: Colour, font_style: str, section_title_colour: Colour) -> None:
+        """Initializes a new RecommendationDisplay object.
+        """
         self.screen = screen
         self.position = (
             screen.get_width() * (1 - width_percentage) + 20, screen.get_height() * top_bar_height_percentage)
@@ -411,7 +457,10 @@ class RecommendationDisplay:
         self.font_style = font_style
         self.section_title_colour = section_title_colour
 
-    def draw(self):
+    def draw(self) -> None:
+        """A method that draws the RecommendationDisplay according to it's 
+        font and stored title colour, etc.
+        """
         # Section title
         title_surf = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         img = self.anime_font.render('Recommendations', True, self.section_title_colour)
@@ -421,7 +470,7 @@ class RecommendationDisplay:
 
     def update(self, animes: list[Anime], spotlight: AnimeSpotlight) -> dict[str: tuple[Anime, Button]]:
         """Updates recommendations according to list of anime provided and returns the buttons created in a dict mapping
-        the anime title to its button on the display. This makes checking for button clicks possible
+        the anime title to its button on the display. This makes checking for button clicks possible.
         """
         # potentially sort them
         button_height = (self.height - 2 * self.margin - self.title_height) / len(animes)
@@ -449,6 +498,8 @@ class RecommendationDisplay:
         return anime_buttons
 
     def shorten_title(self, text) -> str:
+        """A method that shortens the title displayed in pygame.
+        """
         render = self.anime_font.render(text, True, (0, 0, 0))
         render_width = render.get_width()
 
@@ -463,18 +514,28 @@ class RecommendationDisplay:
 
 
 class PreferenceMeter(Button):
+    """
+    An object that represents a preference meter where users
+    can indicate their preference for a category over another(
+    between ['STORY', 'ANIMATION', 'SOUND', 'CHARACTER']).
+    """
     value: float | int
     meter_colour: Colour
     curr_meter_rect: Optional[pygame.Rect]
 
     def __init__(self, screen: pygame.Surface, height: Coord, width: Coord, position: Position, colour: Colour,
                  border_colour: Colour, meter_colour: Colour) -> None:
+        """
+        Initialize a new PreferenceMeter object.
+        """
         Button.__init__(self, screen, height, width, position, '', colour, colour, colour, border_colour=border_colour)
         self.meter_colour = meter_colour
         self.value = 1
         self.curr_meter_rect = None
 
     def draw(self) -> None:
+        """Draw a button representing the PreferenceMeter object. 
+        """
         btn_rect = pygame.Rect(self.position, (self._width, self._height))
         pygame.draw.rect(self._screen, self._current_colour, btn_rect)
         if self.curr_meter_rect is not None:
@@ -483,6 +544,8 @@ class PreferenceMeter(Button):
         self.update((0, 55))
 
     def update(self, mouse_pos: Position) -> None:
+        """Update the drawn button according to the position of the mouse.
+        """
         mouse_y = mouse_pos[1]
         meter_height = self.position[1] + self._height - mouse_y
         # Update the value
@@ -498,6 +561,9 @@ class PreferenceMeter(Button):
 
 
 class PreferenceMeterDisplay:
+    """A class representing the pygame 
+    display for a preference meter.
+    """
     screen: pygame.surface
     height: Coord
     width: Coord
@@ -511,7 +577,9 @@ class PreferenceMeterDisplay:
     text_colour: Colour
 
     def __init__(self, screen: pygame.surface, height_percentage: float, offset_x_percentage: float, colour: Colour,
-                 border_colour: Colour, meter_colour: Colour, text_colour: Colour, font_style: str):
+                 border_colour: Colour, meter_colour: Colour, text_colour: Colour, font_style: str) -> None:
+        """Initialize a new PreferenceMeterDisplay object.
+        """
         self.screen = screen
         self.height = screen.get_height() * height_percentage * 0.6
         self.width = screen.get_width() * 0.03
@@ -525,7 +593,10 @@ class PreferenceMeterDisplay:
         self.text_height = screen.get_height() * height_percentage * 0.7
         self.text_colour = text_colour
 
-    def draw_display(self):
+    def draw_display(self) -> None:
+        """Draw a display for the preference meter with the indicators
+        ['STORY', 'ANIMATION', 'SOUND', 'CHARACTER'].
+        """
         meter_names = ['STORY', 'ANIMATION', 'SOUND', 'CHARACTER']
 
         for i, meter_name in enumerate(meter_names):
@@ -545,6 +616,8 @@ class PreferenceMeterDisplay:
             self.meters[meter_name] = meter
 
     def draw_meter_titles(self) -> None:
+        """A method to draw the titles for each category in the pygame screen.
+        """
         meter_names = ['STORY', 'ANIMATION', 'SOUND', 'CHARACTER']
 
         for i, meter_name in enumerate(meter_names):
@@ -554,6 +627,9 @@ class PreferenceMeterDisplay:
             self.screen.blit(text, (position_x + self.width / 2 - offset_x, self.text_height + 8))
 
     def get_preferences(self) -> dict[str: int]:
+        """Return a dict where the keys are the categories(with 'num-epidodes' included)
+        and the corresponding values are the user's given information.
+        """
         prio = {'story': self.meters['STORY'].value,
                 'animation': self.meters['ANIMATION'].value,
                 'sound': self.meters['SOUND'].value,
@@ -563,6 +639,10 @@ class PreferenceMeterDisplay:
 
 
 class DropDownMenuButton(Button):
+    """
+    A class representing a button on a drop down menu for the
+    user to interact with.
+    """
     selected_colour: Colour
     default_colour: Colour
     is_toggled: bool
@@ -571,6 +651,8 @@ class DropDownMenuButton(Button):
     def __init__(self, screen: pygame.surface, height: Coord, width: Coord, position: Position, genre: str,
                  colour: Colour, selected_colour: Colour, border_colour: Colour, genre_hover_colour: Colour,
                  font_style: str) -> None:
+        """Initialize a DropDownMenuButton object.
+        """
         Button.__init__(self, screen, height, width, position, genre, colour, genre_hover_colour, (0, 0, 0),
                         font_style, border_colour)
         self.selected_colour = selected_colour
@@ -578,7 +660,9 @@ class DropDownMenuButton(Button):
         self.is_toggled = False
         self.original_pos = position
 
-    def update(self):
+    def update(self) -> None:
+        """Update the colour of the button if it is toggled. 
+        """
         self.is_toggled = not self.is_toggled
         if self.is_toggled:
             self._colour = self.selected_colour
